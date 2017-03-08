5bafd3105152e7ee556ccd2e527d415ef3ae9299
Riccardo Rizzo <r.riki@tiscali.it>
2017-03-08 16:22:38 +0000

    Fixed compressor type in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Rails.application.configure do
   config.assets.compress = false #true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false #true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  #config.assets.compile = true
+  #config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Generate digests for assets URLs
   config.assets.digest = true
