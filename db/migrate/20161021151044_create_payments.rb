5a688d26501d8b6975e9c411e4b509936027a632
Riccardo Rizzo <r.riki@tiscali.it>
2017-03-08 09:05:21 +0000

    Fixed compressor type in production

--- db/migrate/20161021151044_create_payments.rb
+++ db/migrate/20161021151044_create_payments.rb
@@ -11,12 +11,5 @@ class CreatePayments < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
-  def change
-      change_column :works, :notification_params
-      change_column :works, :closed
-      change_column :works, :accepted_by
-      change_column :works, :purchased_at
-      change_column :works, :transaction_id
-      change_column :works, :status
-  end
+
 end
